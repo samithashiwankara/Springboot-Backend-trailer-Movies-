652675b33620827453c37156801db6f176fa06d8
Samitha Shiwankara <samithashiwankara46@gmail.com>
2024-02-20 10:20:23 +0530

    initial commit

---
+++ src/main/java/Movie.java
@@ -0,0 +1,39 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collation ="movie")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private Object id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String>genre;
+    private List<String>backdrops;
+    @DocumentReference
+    private List<String>ReviewId;
+
+    public Movie(String imdbId, String title, String releaseDate, String trailerLink, String poster, List<String> backdrops, List<String> genres) {
+        this.imdbId = imdbId;
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.trailerLink = trailerLink;
+        this.poster = poster;
+        this.backdrops = backdrops;
+        this.genre = genres;
+    }
+
+
+}
+
